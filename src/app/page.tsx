27187b40bb26d2ebd0a8741b14ddfd76d5c019c2
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-11-16 23:12:29 +0700

    add auto routing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
+"use client"
+
+import { useRouter } from 'next/navigation';
+import { useCookies } from 'react-cookie';
 
 export default function Home() {
-  return (
-    <h1>Hello</h1>
-  );
+  const [cookies] = useCookies(["user"]);
+  const router = useRouter();
+  if (cookies.user) {
+    router.push('/users');
+  }
+  else {
+    router.push('/login');
+  }
+  return <></>
 }
