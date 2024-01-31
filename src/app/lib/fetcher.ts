5594cbf1b192b6ba4332d08ceafb8d816dc4ac2a
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-01-31 10:46:33 +0700

    feat: add chula-sso

--- src/app/lib/fetcher.ts
+++ src/app/lib/fetcher.ts
@@ -20,10 +20,10 @@ export async function sendRequest2(url: string, ticket: string) {
   return fetch(url, {
     headers: {
       "Content-Type": "application/json",
-      DeeAppId: "app.vercel.sci-locker",
-      DeeAppSecret:
+      "DeeAppId": "app.vercel.sci-locker",
+      "DeeAppSecret":
         "fc42f10ca65ec5a314f3e989dc69a08dc26868814d399c283c5cdb1bce485265ee873fc939305b313df67b155dd29b0a2535c67030fb5fe9e9755007abceace5",
-      DeeTicket: ticket,
+      "DeeTicket": ticket,
     },
     method: "GET",
     mode: "no-cors",
