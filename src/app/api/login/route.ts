92e80f18f699f108d7b3c39824b6b00181552b1f
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-04 03:40:47 +0700

    fix: add service name

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,5 +3,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return NextResponse.redirect('/callback')
+    return NextResponse.json("Insert success");
 }
