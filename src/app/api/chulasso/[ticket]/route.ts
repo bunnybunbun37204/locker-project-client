1cf38c57993c40f8d1c7501681a631ad909cf955
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-04 03:38:59 +0700

    fix: add service name

--- src/app/api/chulasso/[ticket]/route.ts
+++ src/app/api/chulasso/[ticket]/route.ts
@@ -2,10 +2,6 @@
 
 import { NextResponse } from "next/server";
 
-const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const NEXT_PUBLIC_SUPABASE_ANON_KEY =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-
 const serviceValidation = async (ticket: string) => {
   try {
     const url = "https://account.it.chula.ac.th/serviceValidation";
@@ -13,21 +9,22 @@ const serviceValidation = async (ticket: string) => {
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        DeeAppId: "app.vercel.sci-locker",
-        DeeAppSecret:
+        "DeeAppId": "app.vercel.sci-locker",
+        "DeeAppSecret":
           "fc42f10ca65ec5a314f3e989dc69a08dc26868814d399c283c5cdb1bce485265ee873fc939305b313df67b155dd29b0a2535c67030fb5fe9e9755007abceace5",
-        DeeTicket: ticket,
+        "DeeTicket": ticket,
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Headers': "*",
+        'Content-type': 'application/json',
       },
-      mode: "no-cors",
     });
     console.log("finish post");
 
     if (response.ok) {
       const jsonResponse = await response.json()
-      const text = await response.text()
       return {
         status: 200,
-        message: {jsonResponse, text},
+        message: jsonResponse,
       };
     } else {
       console.log("ERROR");
@@ -35,12 +32,9 @@ const serviceValidation = async (ticket: string) => {
       // Handle non-OK response (e.g., 404, 500, etc.)
       console.error(`Error: ${response.status} - ${response.statusText}`);
       const jsonResponse = await response.json()
-      console.log(jsonResponse);
-      
-      const text = await response.text()
       return {
         status: response.status,
-        message: {text, jsonResponse}
+        message: jsonResponse
       };
     }
   } catch (error) {
@@ -54,6 +48,6 @@ const serviceValidation = async (ticket: string) => {
 export async function GET(request: Request) {
   const ticket = request.url.slice(request.url.lastIndexOf("/") + 1);
   const {status, message} = await serviceValidation(ticket);
-  console.log("Message : ",message)
+  console.log("Message : ",message);
   return NextResponse.json(message);
 }
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,5 +3,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return NextResponse.json("Insert success");
+    return NextResponse.redirect('/callback')
 }
