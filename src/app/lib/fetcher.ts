0ade5aedfed507691aaa916d339711994e13be9d
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-03 15:34:39 +0700

    fix: api end point

--- src/app/lib/fetcher.ts
+++ src/app/lib/fetcher.ts
@@ -35,18 +35,10 @@ export async function sendRequest2(url: string, ticket: string) {
 
 export const serviceValidation = async (ticket: string): Promise<string | null> => {
   try {
-    const url = "https://account.it.chula.ac.th/serviceValidation";
+    const url = `https://locker-vidya-api.netlify.app/.netlify/functions/api/locker/login?ticket=${ticket}`;
 
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'DeeAppId': 'app.vercel.sci-locker',
-        'DeeAppSecret': "fc42f10ca65ec5a314f3e989dc69a08dc26868814d399c283c5cdb1bce485265ee873fc939305b313df67b155dd29b0a2535c67030fb5fe9e9755007abceace5",
-        'DeeTicket': ticket,
-      },
-      mode : 'no-cors'
-    });
-    console.log("finish post");
+    const response = await fetch(url);
+    console.log("finish get");
     
     if (response.ok) {
       console.log("OK");
